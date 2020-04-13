bee033ed9ddc2a8f39bd06795199b93f616d100a
LevS503 <semkov01@mail.ru>
2020-04-13 21:23:33 +0500

    commentarii

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include<string>
 
 void main(){
-std::cout<<"enter your name"<<std::endl;
+std::cout<<"enter your name"<<std::endl;//вводим имя
 string name;
 std::cout<<std::endl;
 getline(std::cin, name);
