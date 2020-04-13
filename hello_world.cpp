430e9ec3bf347ba36ca45d074f18277630fae8b1
LevS503 <semkov01@mail.ru>
2020-04-13 22:38:52 +0500

    Mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,12 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
-void main(){
-std::cout<<"enter your name"<<std::endl;//вводим имя
-string name;
-std::cout<<std::endl;
-getline(std::cin, name);
-std::cout<<"Hello world from "<<name;
+void
+main()
+{
+  std::cout << "enter your name" << std::endl; //вводим имя
+  string name;
+  std::cout << std::endl;
+  getline(std::cin, name);
+  std::cout << "Hello world from " << name;
 }
